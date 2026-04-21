3dcefdd13d10761013f74f788182f27536479566
JTANKxz <joetankster@gmail.com>
2026-04-21 09:13:55 -0300

    Adicionado conteudo adulto

--- app/Models/AdultModel.php
+++ app/Models/AdultModel.php
@@ -8,6 +8,10 @@ class AdultModel extends Model
 {
     protected $fillable = ['name', 'slug', 'biography', 'photo_url', 'cover_url', 'instagram', 'twitter', 'is_active'];
 
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+
     public function galleries()
     {
         return $this->hasMany(AdultGallery::class, 'adult_model_id');
---
+++ debug_api.php
@@ -0,0 +1,9 @@
+<?php
+define('LARAVEL_START', microtime(true));
+require __DIR__ . '/vendor/autoload.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    Illuminate\Http\Request::create('/api/adult/home', 'GET')
+);
+echo $response->getContent();
